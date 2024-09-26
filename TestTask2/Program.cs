6e3fa0f7881727270f3ffde485537e65d8bffb07
alpa26 <panovalecsander717@gmail.com>
2024-09-27 01:00:22 +0500

    Initial commit

---
+++ TestTask2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace TestTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Начало");
+            RunTests();
+        }
+
+        public static void RunTests()
+        {
+            // Поток для записи
+            Thread thr1 = new Thread(() =>
+            {
+                for (int i = 0; i < 25; i++)
+                    ReadValue(1);
+            });
+
+            // Потоки для параллельного чтения
+            Thread thr2 = new Thread(() =>
+            {
+                Thread.Sleep(10);
+
+                for (int i = 0; i < 25; i++)
+                    WriteValue(2);
+                
+            });
+
+            Thread thr3 = new Thread(() =>
+            {
+                Thread.Sleep(10);
+
+                for (int i = 0; i < 25; i++)
+                    WriteValue(3);
+            });
+
+            Thread thr4 = new Thread(() =>
+            {
+                Thread.Sleep(10);
+
+                for (int i = 0; i < 25; i++)
+                    WriteValue(3);
+            });
+
+            Thread thr5 = new Thread(() =>
+            {
+                Thread.Sleep(10);
+
+                for (int i = 0; i < 25; i++)
+                    WriteValue(3);
+            });
+
+
+            thr1.Start();
+            thr2.Start();
+            thr3.Start();
+            thr4.Start();
+            thr5.Start();
+        }
+        public static void WriteValue(int thr)
+        {
+            int c = Server.GetCount();
+            //Console.WriteLine($"Финиш чтения потока {thr} {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}, count {c}");
+            Thread.Sleep(20);
+        }
+
+        public static void ReadValue(int thr)
+        {
+            var c = Server.AddToCount(new Random().Next(1, 1000));
+            //Console.WriteLine($"Запись потока {thr} {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}, count {c}");
+        }
+    }
+}
---
+++ TestTask2/Server.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestTask2
+{
+    public static class Server
+    {
+
+        private static volatile int count;
+        private static ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+
+        public static int GetCount()
+        {
+            _lock.EnterReadLock();
+            Console.WriteLine($"Начало чтения переменной {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}, count {count}"); // не произойдет пока происходит запись переменной
+            /*
+              Можно увидеть что чтение потоков происходит одновременно до миллисекунд
+            */
+            try
+            {
+                return count;
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
+
+        public static int AddToCount(int value)
+        {
+            _lock.EnterWriteLock();
+            Console.WriteLine($"  Начало записи потока {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}, count: {value + count}");
+            try
+            {
+                count += value;
+                return count;
+            }
+            finally
+            {
+                Thread.Sleep(200);
+                Console.WriteLine($"  Конец  записи потока {DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff")}, count: {count}");
+                _lock.ExitWriteLock();
+            }
+        }
+    }
+}
